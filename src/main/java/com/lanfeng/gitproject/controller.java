6560d984071e6abcbde28b10710f0e7c0af5ad97
Ludanr <jxlanfeng22@163.com>
2020-06-12 23:49:13 +0800

    第二次主线提交

--- src/main/java/com/lanfeng/gitproject/controller.java
+++ src/main/java/com/lanfeng/gitproject/controller.java
@@ -7,5 +7,6 @@ package com.lanfeng.gitproject;
 public class controller {
     public static void main(String[] args) {
         System.out.println("第一次主线提交");
+        System.out.println("主线第二次提交");
     }
 }

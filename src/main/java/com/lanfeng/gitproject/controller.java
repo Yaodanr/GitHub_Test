9a9defc5e9d27012965064a4ae27858aebf303e5
Ludanr <jxlanfeng22@163.com>
2020-06-13 00:29:03 +0800

    开发分支更新模块一

--- src/main/java/com/lanfeng/gitproject/controller.java
+++ src/main/java/com/lanfeng/gitproject/controller.java
@@ -8,5 +8,6 @@ public class controller {
     public static void main(String[] args) {
         System.out.println("第一次主线提交");
         System.out.println("主线第二次提交");
+        System.out.println("开发更新模块一");
     }
 }
